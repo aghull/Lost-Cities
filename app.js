fd89527f7f158070c8b0e763c0768856e7625021
Andrew Hull <aghull@gmail.com>
2012-01-02 14:40:08 -0500

    port for heroku

--- app.js
+++ app.js
@@ -88,5 +88,6 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
